3aeeac85eb231bb2d479a060e9c2691684992882
leif.lodahl <leif.lodahl@4e58de57-8926-0410-947e-8945c843cdd7>
2012-08-30 10:54:07 +0000

    Modifications to Apache setup

--- DokumentBroker/trunk/document_broker/document_broker/wsgi.py
+++ DokumentBroker/trunk/document_broker/document_broker/wsgi.py
@@ -16,13 +16,12 @@ framework.
 import os
 import sys
 
-from util.helpers import get_install_dir
-
-
-install_dir = get_install_dir()
-sys.path.append(install_dir)
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "document_broker.settings")
+
+install_dir = os.path.abspath(
+    os.path.join( os.path.dirname(__file__), '..')
+)
+sys.path.append(install_dir)
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
