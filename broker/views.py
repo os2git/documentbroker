913141a8e26144c578c094a1e01fb16bec0c2d9d
leif.lodahl <leif.lodahl@4e58de57-8926-0410-947e-8945c843cdd7>
2012-08-30 10:54:04 +0000

    Introduced client system authorization and UUID lookup from name

--- broker/views.py
+++ broker/views.py
@@ -2,9 +2,19 @@
 
 # TODO: Factor out user authentication to a decorator.
 
+
 from models import DocumentBroker
+from utils import authorized
+
+from configuration.models import ClientSystem
 
 
+def get_client_systems():
+    client_systems = ClientSystem.objects.all()
+    return [(c.name, c.uuid) for c in client_systems]
+
+
+@authorized
 def generate(user_authentication, template_id, fields):
     """Gets template from template server and generates document.
     Store document in suitable place and return URL to retrieve it."""
