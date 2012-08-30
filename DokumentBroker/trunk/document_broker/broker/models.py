8cfe70fccc2febe81cc5507acffc159956d98436
leif.lodahl <leif.lodahl@4e58de57-8926-0410-947e-8945c843cdd7>
2012-08-30 10:54:03 +0000

    Found structure for the broker app

--- DokumentBroker/trunk/document_broker/broker/models.py
+++ DokumentBroker/trunk/document_broker/broker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class DocumentBroker(object):
+    """This class is not really a Django model. Rather, it implements a proxy
+    which serves as a unified front for all the document-related functionality
+    we need to support from the views. Thus, this is the "model" for the
+    document broker, template retrieval and similar functionality which we need
+    to handle the requests received by the views. 
+    
+    Whereas the view functions may be protocol dependent, this model class is
+    not.
+    """
+
+    def __init__(self, user_system_id):
+        self._user_system_id = user_system_id
+
+
+    def generate_document(self, template_id, fields):
+        """Return URL to the generated document - throw exception if not
+        possible."""
+        # Check user system is allowed to use template.
+        # Then retrieve template from template server.
+        # Finally, store template in appropriate place, generate document,
+        # store document in appropriate place and return URL.
+        raise RuntimeError("Not implemented: {0}".format(template_id))
+        return ""
+
