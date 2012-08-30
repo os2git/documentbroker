fbcb21eb1d220c1ea414dc9acb78846d70bd0249
leif.lodahl <leif.lodahl@4e58de57-8926-0410-947e-8945c843cdd7>
2012-08-30 10:54:03 +0000

    Found structure for the broker app

--- broker/views.py
+++ broker/views.py
@@ -2,10 +2,16 @@
 
 # TODO: Factor out user authentication to a decorator.
 
+from models import DocumentBroker
+
+
 def generate(user_authentication, template_id, fields):
     """Gets template from template server and generates document.
     Store document in suitable place and return URL to retrieve it."""
-    # TODO: Implement this
-    return "http://fremogtilbageerligelangt.dk"
+    # TODO: CHECK USER SYSTEM IS AUTHORIZED!
+
+    broker = DocumentBroker(user_authentication[0])
+
+    broker.generate_document(template_id, fields)
 
 
