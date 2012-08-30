af7654dc44fa3bfb4cc0dbc67af3acfaff070782
leif.lodahl <leif.lodahl@4e58de57-8926-0410-947e-8945c843cdd7>
2012-08-30 10:54:10 +0000

    Now validate template against template server - one step further

--- broker/models.py
+++ broker/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 
+from client.template_client import TemplateServer
+from client.settings import TEMPLATE_URL
 
 class DocumentBroker(object):
     """This class is not really a Django model. Rather, it implements a proxy
@@ -20,7 +21,19 @@ class DocumentBroker(object):
         """Return URL to the generated document - throw exception if not
         possible."""
         # Check user system is allowed to use template.
+        ts = TemplateServer(TEMPLATE_URL)
+        templates = ts.get_templates(self._user_system_id)
+
+        if template_id in [ t[0] for t in templates]:
+            # TODO: This uses name not ID, change it later.
+            url = { t[0] : t[2] for t in templates }[template_id]
+        else:
+            url = None
+        raise RuntimeError(str(templates) + url)
         # Then retrieve template from template server.
+        if url:
+            """ Get template file from URL. Cache? Start by retrieving
+            to file buffer """
         # Finally, store template in appropriate place, generate document,
         # store document in appropriate place and return URL.
         raise RuntimeError("Not implemented: {0}".format(template_id))
