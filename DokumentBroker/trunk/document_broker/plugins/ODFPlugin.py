c0a4f3bb338cf8c7fa63a48614779ce5699b62c4
leif.lodahl <leif.lodahl@4e58de57-8926-0410-947e-8945c843cdd7>
2012-08-30 10:54:00 +0000

    Added first version of ODF generator

--- DokumentBroker/trunk/document_broker/plugins/ODFPlugin.py
+++ DokumentBroker/trunk/document_broker/plugins/ODFPlugin.py
@@ -1,7 +1,9 @@
 """This file implements the ODF generator plugin for the Document Broker."""
 
-from util.document_plugin import DocumentPlugin
+import sys
 
+from util.document_plugin import DocumentPlugin
+from util.odf_generator import generate_odf
 
 class ODFPlugin(DocumentPlugin):
 
@@ -13,6 +15,26 @@ class ODFPlugin(DocumentPlugin):
         """Generate ODF document from template.
 
         Log error and possibly throw exception in case of failure."""
-        pass
+        try:
+            generate_odf(template_file, fields, output_path)
+        except Exception as e:
+            # TODO: Proper error logging here, many thanks!
+            sys.stderr.write("Error found: " + str(e))
+
+
+
+if __name__ == '__main__':
+    
+
+    fields = {}
+    fields['Leif'] = ('LeifLeifLeif', 'string')
+    fields['Dato'] = ('2012-05-11', 'date')
+    fields['Svaret'] = ('137', 'string')
+    #fields['document_creator'] = ('Document Broker ODF Plugin', 'string')
+
+ 
+    odf_plugin = ODFPlugin()
+    odf_plugin.generate("/home/carstena/src/document_broker/research/Test.ott",
+                        "output.odf", fields)
 
 
